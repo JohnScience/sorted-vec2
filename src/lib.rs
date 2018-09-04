0ed9b041967cd453eab646862251db8971b4c7d0
Shane Pearman <spearman@gitlab.com>
2018-09-03 22:25:21 -0700

    0.2.1: add reverse sorted types

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ pub struct SortedVec <T : Ord> {
   vec : Vec <T>
 }
 
+#[derive(Clone,Debug,PartialEq)]
+pub struct ReverseSortedVec <T : Ord> {
+  vec : Vec <T>
+}
+
 impl <T : Ord> SortedVec <T> {
   #[inline]
   pub fn new() -> Self {
@@ -32,8 +37,8 @@ impl <T : Ord> SortedVec <T> {
     }
   }
   #[inline]
-  pub fn remove_item (&mut self, element : &T) -> Option <T> {
-    match self.vec.binary_search (element) {
+  pub fn remove_item (&mut self, item : &T) -> Option <T> {
+    match self.vec.binary_search (item) {
       Ok  (remove_at) => Some (self.vec.remove (remove_at)),
       Err (_)         => None
     }
@@ -55,14 +60,78 @@ impl <T : Ord> SortedVec <T> {
     self.vec.dedup();
   }
 }
-
 impl <T : Ord> Default for SortedVec <T> {
   fn default() -> Self {
     Self::new()
   }
 }
-
 impl <T : Ord> std::ops::Deref for SortedVec <T> {
+  type Target = Vec <T>;
+  fn deref (&self) -> &Vec <T> {
+    &self.vec
+  }
+}
+
+impl <T : Ord> ReverseSortedVec <T> {
+  #[inline]
+  pub fn new() -> Self {
+    ReverseSortedVec { vec: Vec::new() }
+  }
+  #[inline]
+  pub fn with_capacity (capacity : usize) -> Self {
+    ReverseSortedVec { vec: Vec::with_capacity (capacity) }
+  }
+  /// Insert an element into (reverse) sorted position, returning the order
+  /// index at which it was placed.
+  ///
+  /// If the element was already present, the order index is returned as an
+  /// `Err`, otherwise it is returned with `Ok`.
+  pub fn insert (&mut self, element : T) -> Result <usize, usize> {
+    match &self.vec[..].binary_search_by (
+      |other_element| other_element.cmp (&element).reverse()
+    ) {
+      Ok  (insert_at) => {
+        self.vec.insert (*insert_at, element);
+        Err (*insert_at)
+      }
+      Err (insert_at) => {
+        self.vec.insert (*insert_at, element);
+        Ok  (*insert_at)
+      }
+    }
+  }
+  #[inline]
+  pub fn remove_item (&mut self, item : &T) -> Option <T> {
+    match self.vec.binary_search_by (
+      |other_item| other_item.cmp (&item).reverse()
+    ) {
+      Ok  (remove_at) => Some (self.vec.remove (remove_at)),
+      Err (_)         => None
+    }
+  }
+  /// Panics if index is out of bounds
+  #[inline]
+  pub fn remove_index (&mut self, index : usize) -> T {
+    self.vec.remove (index)
+  }
+  #[inline]
+  pub fn pop (&mut self) -> Option <T> {
+    self.vec.pop()
+  }
+  #[inline]
+  pub fn clear (&mut self) {
+    self.vec.clear()
+  }
+  pub fn dedup (&mut self) {
+    self.vec.dedup();
+  }
+}
+impl <T : Ord> Default for ReverseSortedVec <T> {
+  fn default() -> Self {
+    Self::new()
+  }
+}
+impl <T : Ord> std::ops::Deref for ReverseSortedVec <T> {
   type Target = Vec <T>;
   fn deref (&self) -> &Vec <T> {
     &self.vec
@@ -83,5 +152,18 @@ mod tests {
     assert_eq!(v.len(), 4);
     v.dedup();
     assert_eq!(v.len(), 3);
+  }
+
+  #[test]
+  fn test_reverse_sorted_vec() {
+    let mut v = ReverseSortedVec::new();
+    assert_eq!(v.insert (5), Ok (0));
+    assert_eq!(v.insert (3), Ok (1));
+    assert_eq!(v.insert (4), Ok (1));
+    assert_eq!(v.insert (6), Ok (0));
+    assert_eq!(v.insert (4), Err (2));
+    assert_eq!(v.len(), 5);
+    v.dedup();
+    assert_eq!(v.len(), 4);
   }
 }
--- src/partial.rs
+++ src/partial.rs
@@ -5,6 +5,16 @@ pub struct SortedVec <T : PartialOrd> {
   vec : Vec <T>
 }
 
+#[derive(Clone,Debug,PartialEq)]
+pub struct ReverseSortedVec <T : PartialOrd> {
+  vec : Vec <T>
+}
+
+/// Unwraps a `partial_cmp`
+fn partial_compare <T : PartialOrd> (lhs : &T, rhs : &T) -> std::cmp::Ordering {
+  lhs.partial_cmp (rhs).unwrap()
+}
+
 impl <T : PartialOrd> SortedVec <T> {
   #[inline]
   pub fn new() -> Self {
@@ -23,7 +33,7 @@ impl <T : PartialOrd> SortedVec <T> {
   /// Partial order comparison panics if items are not comparable.
   pub fn insert (&mut self, element : T) -> Result <usize, usize> {
     match &self.vec[..].binary_search_by (
-      |other_element| Self::partial_compare (other_element, &element)
+      |other_element| partial_compare (other_element, &element)
     ) {
       Ok  (insert_at) => {
         self.vec.insert (*insert_at, element);
@@ -38,7 +48,7 @@ impl <T : PartialOrd> SortedVec <T> {
   #[inline]
   pub fn remove_item (&mut self, item : &T) -> Option <T> {
     match self.vec.binary_search_by (
-      |other_item| Self::partial_compare (other_item, item)
+      |other_item| partial_compare (other_item, item)
     ) {
       Ok  (remove_at) => Some (self.vec.remove (remove_at)),
       Err (_)         => None
@@ -61,21 +71,79 @@ impl <T : PartialOrd> SortedVec <T> {
   pub fn dedup (&mut self) {
     self.vec.dedup();
   }
-  //
-  //  private methods
-  //
-  fn partial_compare (lhs : &T, rhs : &T) -> std::cmp::Ordering {
-    lhs.partial_cmp (rhs).unwrap()
-  }
 }
-
 impl <T : PartialOrd> Default for SortedVec <T> {
   fn default() -> Self {
     Self::new()
   }
 }
-
 impl <T : PartialOrd> std::ops::Deref for SortedVec <T> {
+  type Target = Vec <T>;
+  fn deref (&self) -> &Vec <T> {
+    &self.vec
+  }
+}
+
+impl <T : PartialOrd> ReverseSortedVec <T> {
+  #[inline]
+  pub fn new() -> Self {
+    ReverseSortedVec { vec: Vec::new() }
+  }
+  #[inline]
+  pub fn with_capacity (capacity : usize) -> Self {
+    ReverseSortedVec { vec: Vec::with_capacity (capacity) }
+  }
+  /// Insert an element into (reverse) sorted position, returning the order
+  /// index at which it was placed.
+  ///
+  /// If the element was already present, the order index is returned as an
+  /// `Err`, otherwise it is returned with `Ok`.
+  pub fn insert (&mut self, element : T) -> Result <usize, usize> {
+    match &self.vec[..].binary_search_by (
+      |other_element| partial_compare (other_element, &element).reverse()
+    ) {
+      Ok  (insert_at) => {
+        self.vec.insert (*insert_at, element);
+        Err (*insert_at)
+      }
+      Err (insert_at) => {
+        self.vec.insert (*insert_at, element);
+        Ok  (*insert_at)
+      }
+    }
+  }
+  #[inline]
+  pub fn remove_item (&mut self, item : &T) -> Option <T> {
+    match self.vec.binary_search_by (
+      |other_item| partial_compare (other_item, item).reverse()
+    ) {
+      Ok  (remove_at) => Some (self.vec.remove (remove_at)),
+      Err (_)         => None
+    }
+  }
+  /// Panics if index is out of bounds
+  #[inline]
+  pub fn remove_index (&mut self, index : usize) -> T {
+    self.vec.remove (index)
+  }
+  #[inline]
+  pub fn pop (&mut self) -> Option <T> {
+    self.vec.pop()
+  }
+  #[inline]
+  pub fn clear (&mut self) {
+    self.vec.clear()
+  }
+  pub fn dedup (&mut self) {
+    self.vec.dedup();
+  }
+}
+impl <T : PartialOrd> Default for ReverseSortedVec <T> {
+  fn default() -> Self {
+    Self::new()
+  }
+}
+impl <T : PartialOrd> std::ops::Deref for ReverseSortedVec <T> {
   type Target = Vec <T>;
   fn deref (&self) -> &Vec <T> {
     &self.vec
@@ -96,5 +164,18 @@ mod tests {
     assert_eq!(v.len(), 4);
     v.dedup();
     assert_eq!(v.len(), 3);
+  }
+
+  #[test]
+  fn test_reverse_sorted_vec() {
+    let mut v = ReverseSortedVec::new();
+    assert_eq!(v.insert (5.0), Ok (0));
+    assert_eq!(v.insert (3.0), Ok (1));
+    assert_eq!(v.insert (4.0), Ok (1));
+    assert_eq!(v.insert (6.0), Ok (0));
+    assert_eq!(v.insert (4.0), Err (2));
+    assert_eq!(v.len(), 5);
+    v.dedup();
+    assert_eq!(v.len(), 4);
   }
 }
